952e15c1f9cf0a845e64a3d4d61cc04083aa0252
cmorenogonzalez33 <94968881+cmorenogonzalez33@users.noreply.github.com>
2026-04-19 22:28:19 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
- flask import Flask, render_template, request
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
